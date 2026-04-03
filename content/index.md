a29f096a8ab3d766cabf199019c2456227211edf
angelo-francisco <ics20080729@gmail.com>
2026-04-03 07:35:37 +0100

    aa

---
+++ content/index.md
@@ -0,0 +1,5 @@
+Title: My first try: a new journey
+Date: 2026-04-03 10:20
+Category: Coding
+
+My first blog page
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+AUTHOR = 'Ângelo Francisco'
+SITENAME = "Ângelo's Blog"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Africa/Luanda'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+LINKS = (
+    ("Home", "/"),
+)
+
+SOCIAL = (
+    ("Github", "https://github.com/angelo-francisco/"),
+    ("LinkedIn", "https://getpelican.com/"),
+)
+
+DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = ["images", "extra"]
+
+EXTRA_PATH_METADATA = {
+    "extra/favicon.ico": {"path": "favicon.png"},
+}
